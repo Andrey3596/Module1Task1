6d9558eed219fdbc56ddb1004b811e420d561da1
Limolix <andrejnagirnak395@gmail.com>
2026-02-23 23:59:56 +0800

    add if

--- task1/Program.cs
+++ task1/Program.cs
@@ -4,3 +4,16 @@ var num = int.Parse(Console.ReadLine());
 int x = num / 100;
 int y = (num - x * 100) / 10;
 int z = (num - x * 100 - y * 10);
+
+if (x > y && x > z)
+{
+    Console.WriteLine(x);
+}
+else if (y > z && y > x)
+{
+    Console.WriteLine(y);
+}
+else
+{
+    Console.WriteLine(z);
+}
