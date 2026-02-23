7077cdc8b98dbb8c0437a6c29f4fb4562ef3da5e
Limolix <andrejnagirnak395@gmail.com>
2026-02-24 00:08:12 +0800

    add dec

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,19 +1,33 @@
-﻿Console.WriteLine("Введите трех значное число");
-var num = int.Parse(Console.ReadLine());
+﻿namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите трех значное число");
+            var num = int.Parse(Console.ReadLine());
 
-int x = num / 100;
-int y = (num - x * 100) / 10;
-int z = (num - x * 100 - y * 10);
+            int x = num / 100;
+            int y = (num - x * 100) / 10;
+            int z = (num - x * 100 - y * 10);
 
-if (x > y && x > z)
-{
-    Console.WriteLine(x);
-}
-else if (y > z && y > x)
-{
-    Console.WriteLine(y);
-}
-else
-{
-    Console.WriteLine(z);
+            int answer;
+
+            if (x > y && x > z)
+            {
+                answer = x;
+            }
+            else if (y > z && y > x)
+            {
+                answer = y;
+            }
+            else
+            {
+                answer = z;
+            }
+
+            Console.WriteLine(answer);
+            
+        }
+    }
 }
