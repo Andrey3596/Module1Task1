6dde7d92169050b48054d4fe9be04bbefa50fa81
Limolix <andrejnagirnak395@gmail.com>
2026-02-23 23:56:29 +0800

    write data

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите трех значное число");
+var num = int.Parse(Console.ReadLine());
+
+int x = num / 100;
+int y = (num - x * 100) / 10;
+int z = (num - x * 100 - y * 10);
